86c5016880d077b63918b2c6906278a6d56b9977
Cautious Engineer <jamaalwbrown@utexas.edu>
2022-05-27 23:49:04 -0500

    Add coding challenges for 5-26 and 5-28'

--- Codewars 05-26-22.js
+++ Codewars 05-26-22.js
@@ -40,5 +40,6 @@
 const binaryArrayToNumber = arr => {
     return parseInt(arr.join(''), 2);
   };
+  
 //----------BEST PRACTICES SOLUTION----------//
-const binaryArrayToNumber = arr => parseInt(arr.join(''), 2);
+//const binaryArrayToNumber = arr => parseInt(arr.join(''), 2);
