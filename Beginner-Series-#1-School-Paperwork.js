b247c074716f5360153711349a5dd75f5608230d
Cautious Engineer <jamaalwbrown@utexas.edu>
2022-06-10 09:07:23 -0500

    Update Beginner-Series-#1-School-Paperwork.js

--- Beginner-Series-#1-School-Paperwork.js
+++ Beginner-Series-#1-School-Paperwork.js
@@ -10,14 +10,29 @@
 
 //PREP
 //Parameters: 
+//2 parameters. n which is the number of classmates(number). m which is how many pages the paperwork has (number).
 
 //Return: 
+//The number of pages you have to copy (number). If n < 0 and m < 0 return 0
 
 //Constraints: 
- 
-//Examples: 
+//We can expect some cases where n and m is negative
+//
+
+//Examples:
+// n= 5, m=5: 25
+// n=-5, m=5:  0
   
 //Psuedocode:
+//Check if both n and m is 0. If not true then return n * m;
+
+function paperwork(n, m) {
+    if(n < 0 || m < 0) {
+      return 0;
+    }
+    else {
+      return n * m;
+    }
+  }
    
   
-//----------BEST PRACTICES SOLUTION----------//
