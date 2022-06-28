88399f99f2118092a8c70959cf8cfd4d00a9240d
Cautious Engineer <jamaalwbrown@utexas.edu>
2022-06-28 14:13:38 -0500

    Add 6 kyu Update-On-Order.js

---
+++ 6 kyu/Unique-In-Order.js
@@ -0,0 +1,22 @@
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+//-----------MY SOLUTION--------------//
+
+//PREP
+//Parameters: A sequence of elements (can be an array or string)
+
+//Return: An array of the unique elements from the passed in string/array
+
+//Constraints: 
+//We need to be able to handle array or string input
+//We should plan to handle any type of element (number, char, etc.)
+ 
+//Examples: 
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+  
+//Psuedocode:
+   
+  
+//----------BEST PRACTICES SOLUTION----------//
