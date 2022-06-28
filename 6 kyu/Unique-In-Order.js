bebd1d4bbd8708aafe23939e1f81f50b92b18c3d
Cautious Engineer <jamaalwbrown@utexas.edu>
2022-06-28 14:39:54 -0500

    Update Unique-In-Order.js

--- 6 kyu/Unique-In-Order.js
+++ 6 kyu/Unique-In-Order.js
@@ -17,6 +17,28 @@
 // uniqueInOrder([1,2,2,3,3])       == [1,2,3]
   
 //Psuedocode:
-   
-  
-//----------BEST PRACTICES SOLUTION----------//
+//1. Check if array or string is empty (e.g if the length property is 0)
+//2. Take the array or string and access the first element. Push that element onto our result Array to be returned
+//3. Iterate through the array or string while checking each element against the current element we are holding. When there is a change grab the new element and push it onto our result array
+//4  We repeat steps 2 and 3 until we reach the end of the array or string
+//5. We return the result Array
+var uniqueInOrder=function(iterable){
+    if (iterable.length === 0) {
+      return []
+    }
+    
+    let index = 0
+    let element = iterable[index];
+    const resultArr = []
+    resultArr.push(element);
+    
+    while(index < iterable.length) {
+      
+      if(element !== iterable[index]) {
+        element = iterable[index];
+        resultArr.push(element);
+      }
+      index++
+    }
+    return resultArr
+  }
