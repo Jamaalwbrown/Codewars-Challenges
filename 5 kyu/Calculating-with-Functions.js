dabac401d1c4a2b77cd617efd03d38e7966a86c2
Cautious Engineer <jamaalwbrown@utexas.edu>
2022-07-11 22:40:43 -0500

    Update Calculating-with-Functions.js

--- 5 kyu/Calculating-with-Functions.js
+++ 5 kyu/Calculating-with-Functions.js
@@ -23,6 +23,10 @@
 //Constraints: 
  
 //Examples: 
+// seven(times(five())); // must return 35
+// four(plus(nine())); // must return 13
+// eight(minus(three())); // must return 5
+// six(dividedBy(two())); // must return 3
   
 //Psuedocode:
    
