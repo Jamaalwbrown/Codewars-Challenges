fa1ec86cfe9c8e8224ebf71295cf4b3e4ac5cfd4
Cautious Engineer <jamaalwbrown@utexas.edu>
2022-10-05 22:44:50 -0500

    Add DNA-RNA-Conversion.js

---
+++ DNA-to-RNA-Conversion.js
@@ -0,0 +1,33 @@
+/*
+Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. 
+It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. 
+In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+Create a function which translates a given DNA string into RNA.
+
+For example:
+
+"GCAT"  =>  "GCAU"
+The input string can be of arbitrary length - in particular, it may be empty. 
+All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+//PREP
+//Parameters: 
+// What are they giving me? What are the expected arguments? Will it only ever be one type of data, or do I need to account for others? 
+// Will there ever be an invalid input? Do I need to account for unexpected edge cases?
+
+//Return: 
+// What do they want me to return? Are they expecting a string? array? object? Is there a certain format the return is expected in? 
+// Will the expected return ever change for invalid inputs or edge cases?
+
+//Examples: 
+// Check your understanding! Are tests provided that you can reference as examples? What would happen in invalid or edge situations? 
+// Aim for at least 3 examples.
+  
+//Psuedocode:
+// In fairly plain speech, how would you solve this? What needs to happen to get from the starting inputs to the desired return? 
+// Think it through step by step, if you write something down and it is more than a single action, you may need to break it down more. 
+// Good pseudocode only comes with practice.
+*/
